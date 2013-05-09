b1075d0ba4cbe5e30b6040ec642ce1fb8578b131
Dan C <spo0kye@yahoo.com>
2013-05-09 14:39:48 +0300

    Roadmap item #1366
    Roadmap item #1366
    http://www.8.php-fusion.net/infusions/roadmap/view.php?item_id=1366

--- includes/user_fields/user_web_include.php
+++ includes/user_fields/user_web_include.php
@@ -36,12 +36,7 @@ if ($profile_method == "input") {
 		echo "<tr>\n";
 		echo "<td class='tbl1'>".$locale['uf_web']."</td>\n";
 		echo "<td align='right' class='tbl1'>";
-		if (!strstr($user_data['user_web'], "http://") && !strstr($user_data['user_web'], "https://")) {
-			$urlprefix = "http://";
-		} else {
-			$urlprefix = "";
-		}
-		echo "<a href='".$urlprefix.$user_data['user_web']."' title='".$urlprefix.$user_data['user_web']."' target='_blank'>".$locale['uf_web_001']."</a>\n";
+		echo "<a href='".$user_data['user_web']."' title='".$user_data['user_web']."' target='_blank'>".$locale['uf_web_001']."</a>\n";
 		echo "</td>\n</tr>\n";
 	}
 
@@ -50,7 +45,11 @@ if ($profile_method == "input") {
 	// Get input data
 	if (isset($_POST['user_web']) && ($_POST['user_web'] != "" || $this->_isNotRequired("user_web"))) {
 		// Set update or insert user data
-		$this->_setDBValue("user_web", stripinput($_POST['user_web']));
+		$user_web = stripinput($_POST['user_web']);
+		if (!preg_match("#^http(s)?://#i", $user_web)) {
+			$user_web = "http://".$user_web;
+		}
+		$this->_setDBValue("user_web", $user_web);
 	} else {
 		$this->_setError("user_web", $locale['uf_web_error'], true);
 	}
