20d74f4f527e21de10e7c2df811c2faf228de4b8
Dan C <spo0kye@yahoo.com>
2013-05-13 13:36:33 +0300

    index.php as starting page [Roadmap item #1220]
    Problem with index.php as starting page [Roadmap item #1220]
    http://www.8.php-fusion.net/infusions/roadmap/view.php?item_id=1220

--- index.php
+++ index.php
@@ -17,7 +17,10 @@
 +--------------------------------------------------------*/
 require_once "maincore.php";
 
-redirect($settings['opening_page']);
+if (empty($settings['opening_page']) || $settings['opening_page'] == "index.php" || $settings['opening_page'] == "/") {
+	redirect("news.php");
+} else {
+	redirect($settings['opening_page']);
+}
 
-mysql_close($db_connect);
 ?>
